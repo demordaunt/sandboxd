14804e79301c0fe7c65b9c8ee144b7b9a630902c
Carter Brown <84643397+demordaunt@users.noreply.github.com>
2022-10-27 11:56:18 -0600

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import pyrebase
+
+
 config={
 	"apiKey": "AIzaSyBcGJ9d3QOHuH_K2G3Gqwu7zgcr1I6a2MM",
 	"authDomain": "sandbox-ec83f.firebaseapp.com",
@@ -9,9 +11,11 @@ config={
 	"messagingSenderId": "342827697815",
 	"appId": "1:342827697815:web:a6ff5093e839f1ff070bd2"
 }
+
 firebase=pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
+
 def home(request):
 	day = database.child('Data').child('Day').get().val()
 	id = database.child('Data').child('Id').get().val()
