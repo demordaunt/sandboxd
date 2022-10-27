177b997486c804fc420dc9dec9ae07fe31da399e
bstone42 <benstone34@gmail.com>
2022-10-27 16:47:50 -0600

    Updates

--- sandboxd/views.py
+++ sandboxd/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 import pyrebase
-
-
 config={
 	"apiKey": "AIzaSyBcGJ9d3QOHuH_K2G3Gqwu7zgcr1I6a2MM",
 	"authDomain": "sandbox-ec83f.firebaseapp.com",
-	"databaseURL": "https://sandboxd.firebasedatabase.app",
+	"databaseURL": "https://sandbox-ec83f-default-rtdb.firebaseio.com/",
 	"projectId": "sandbox-ec83f",
 	"storageBucket": "sandbox-ec83f.appspot.com",
 	"messagingSenderId": "342827697815",
 	"appId": "1:342827697815:web:a6ff5093e839f1ff070bd2"
 }
-
 firebase=pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
-
 def home(request):
 	day = database.child('Data').child('Day').get().val()
 	id = database.child('Data').child('Id').get().val()
 	projectname = database.child('Data').child('Projectname').get().val()
 	return render(request,"Home.html",{"day":day,"id":id,"projectname":projectname })
+
+
